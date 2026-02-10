58745aa314897b13755780dc9ad92603013711ef
taovuzu <yatindanger0001@gmail.com>
2026-02-11 00:51:53 +0530

    add monotonic stack template

---
+++ raider/templates/monotonic_stack.cpp
@@ -0,0 +1,16 @@
+void monotonicStack(const vector<int>& arr) {
+    stack<int> st;
+    for (int i = 0; i < arr.size(); ++i) {
+			while(STACK NOT EMPTY  &&   st.top() OPERATOR arr[i] && other Logic given in problem ){
+					//Extract the stack top
+					//POP  the element 
+					
+					//Use the current element and STACK TOP to update result
+					
+					}
+			
+			//Push in stack
+    }
+	
+	//at this point we have a monotonic stack in our hand
+}
